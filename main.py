6110e1d2e11ef49c46d301dede42f3ebc6808797
Igor <io.shestopalov@gmail.com>
2021-02-04 15:23:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from functions import spreads, plot_chart
+import streamlit as st
+import ccxt
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+import warnings
+warnings.filterwarnings('ignore')
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+okex = ccxt.okex({'type': 'futures'})
+markets = okex.fetch_markets()
+
+st.header('THE BEST ANALYTICAL WEBSITE EVER')
+
+if __name__ == '__main__':
+
+    spreads = spreads()
+
+    btc_spread = pd.DataFrame(spreads.loc['BTC']).T
+    st.text('BTC price: ' + str(okex.fetch_ticker('BTC/USDC')['last']))
+    st.table(btc_spread)
+
+    st.table(spreads.style.highlight_max())
+
+    if st.button('Show Plots'):
+        st.header('BTC Spreads')
+        try:
+            plot_chart()
+        except:
+            print('error')
